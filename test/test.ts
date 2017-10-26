35d764928fa55af123f31aa0f4cde8cfb2011ef5
Panayot Cankov <panayot.cankov@telerik.com>
2017-10-26 10:25:37 +0300

    Add initial commit, some tests, test coverage etc.

--- test/test.ts
+++ test/test.ts
@@ -1,31 +1,64 @@
 // Reference mocha-typescript's global definitions:
 /// <reference path="../node_modules/mocha-typescript/globals.d.ts" />
 
-import { Unit } from "../src/index";
+import { CSS3Parser, TokenType } from "../src/index";
 import { assert } from "chai";
 
-describe("vanilla bdd", () => {
-    it("test", () => {
+describe("css", () => {
+    describe("tokenize", () => {
+        it("Button { background: red; }", () => {
+            const parser = new CSS3Parser("Button { background: red; }");
+            const tokens = parser.tokenize();
+            assert.deepEqual(tokens, [
+                { type: TokenType.ident, text: "Button" },
+                " ", "{", " ",
+                { type: TokenType.ident, text: "background" },
+                ":", " ",
+                { type: TokenType.ident, text: "red" },
+                ";", " ", "}"
+            ]);
+        });
+        it("@import url(~/app.css); Button { color: orange; }", () => {
+            const parser = new CSS3Parser("@import url(~/app.css); Button { color: orange; }");
+            const tokens = parser.tokenize();
+            assert.deepEqual(tokens, [
+                { type: TokenType.atKeyword, text: "import" },
+                " ",
+                { type: TokenType.url, text: "~/app.css" },
+                ";", " ",
+                { type: TokenType.ident, text: "Button" },
+                " ", "{", " ",
+                { type: TokenType.ident, text: "color" },
+                ":", " ",
+                { type: TokenType.ident, text: "orange" },
+                ";", " ", "}"
+            ]);
+        });
+        it("@keyframes", () => {
+            const css = `@keyframes mymove {
+                from { top: 0px; }
+                to { top: 200px; }
+            }`;
+            const parser = new CSS3Parser(css);
+            const tokens = parser.tokenize();
+            assert.deepEqual(tokens, [
+                { type: TokenType.atKeyword, text: "keyframes" },
+                " ",
+                { type: TokenType.ident, text: "mymove" },
+                " ", "{", " ",
+                { type: TokenType.ident, text: "from" },
+                " ", "{", " ",
+                { type: TokenType.ident, text: "top" },
+                ":", " ",
+                { type: TokenType.dimension, text: "0px" },
+                ";", " ", "}", " ",
+                { type: TokenType.ident, text: "to" },
+                " ", "{", " ",
+                { type: TokenType.ident, text: "top" },
+                ":", " ",
+                { type: TokenType.dimension, text: "200px" },
+                ";", " ", "}", " ", "}"
+            ]);
+        });
     });
-});
-
-suite("vanilla tdd", () => {
-    test("test", () => {
-    });
-});
-
-@suite class UnitTest extends Unit {
-    @test "big is true with big number"() {
-        assert(this.big(200));
-    }
-    @test "big is false with small number"() {
-        assert(!this.big(50));
-    }
-}
-
-suite("nested class suite", () => {
-    @suite class NestedTest {
-        @test "a test"() {
-        }
-    }
 });
